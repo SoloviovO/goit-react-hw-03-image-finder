98a7cf664e56e2ba2258a726f48f2aec8d539a1c
SoloviovO <soloviov.o.m@gmail.com>
2023-02-22 18:04:17 +0200

    done

---
+++ src/components/ImageGallery/ImageGallery.jsx
@@ -0,0 +1,25 @@
+import PropTypes from 'prop-types';
+import css from './ImageGallery.module.css';
+
+import { ImageGalleryItem } from 'components/ImageGalleryItem/ImageGalleryItem';
+
+export const ImageGallery = ({ photos }) => {
+  return (
+    <ul className={css.ImageGallery}>
+      {photos.map(photo => {
+        return (
+          <ImageGalleryItem
+            key={photo.id}
+            webformatURL={photo.webformatURL}
+            largeImageURL={photo.largeImageURL}
+            tags={photo.tags}
+          />
+        );
+      })}
+    </ul>
+  );
+};
+
+ImageGallery.propTypes = {
+  photos: PropTypes.array,
+};
---
+++ src/components/ImageGalleryItem/ImageGalleryItem.jsx
@@ -0,0 +1,41 @@
+import { Component } from 'react';
+import PropTypes from 'prop-types';
+import { Modal } from 'components/Modal/Modal';
+import css from './ImageGalleryItem.module.css';
+
+export class ImageGalleryItem extends Component {
+  state = {
+    isOpen: false,
+  };
+
+  toggleModal = () => {
+    this.setState(prevState => ({ isOpen: !prevState.isOpen }));
+  };
+
+  render() {
+    return (
+      <>
+        <li className={css.ImageGalleryItem} onClick={this.toggleModal}>
+          <img
+            className={css.ImageGalleryItemImage}
+            src={this.props.webformatURL}
+            alt=""
+          />
+        </li>
+        {this.state.isOpen && (
+          <Modal
+            largeImageURL={this.props.largeImageURL}
+            onClose={this.toggleModal}
+            tags={this.props.tags}
+          />
+        )}
+      </>
+    );
+  }
+}
+
+ImageGalleryItem.propTypes = {
+  webformatURL: PropTypes.string,
+  tags: PropTypes.string,
+  largeImageURL: PropTypes.string,
+};
